0248b6eb4543184c7cca97a25fac897e55e18eb2
nahidhasan <nahidhasan@finder-lbs.com>
2018-08-19 13:26:22 +0600

    second Commit

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 module.exports.vatCalculator = function(arr) {
   if (Array.isArray(arr)) {
     if (arr.length > 0) {
-      let NewArray = [];
+      if (!arr.some(isNaN)) {
+        let NewArray = [];
 
-      arr.map(item => {
-        let vat_added = Number(item * (15 / 115)).toFixed(2);
-        NewArray.push(vat_added);
-      });
-      let floatTotalVat = NewArray.reduce((a, b) => Number(a) + Number(b));
-      let totalVat = Number.parseFloat(floatTotalVat).toFixed(2);
+        arr.map(item => {
+          let vat_added = Number(item * (15 / 115)).toFixed(2);
+          NewArray.push(vat_added);
+        });
+        let floatTotalVat = NewArray.reduce((a, b) => Number(a) + Number(b));
+        let totalVat = Number.parseFloat(floatTotalVat).toFixed(2);
 
-      let totalSum = arr.map((a, i) => Number(a) + Number(NewArray[i]));
-      let TotalAll = totalSum.reduce((a, b) => Number(a) + Number(b));
+        let totalSum = arr.map((a, i) => Number(a) + Number(NewArray[i]));
+        let TotalAll = totalSum.reduce((a, b) => Number(a) + Number(b));
 
-      return {
-        PreviousItemsPrices: arr,
-        vatOnEachItem: NewArray,
-        TotalVat: totalVat,
-        TotalSumEachItems: totalSum,
-        All: TotalAll
-      };
+        return {
+          PreviousItemsPrices: arr,
+          vatOnEachItem: NewArray,
+          TotalVat: totalVat,
+          TotalSumEachItems: totalSum,
+          All: TotalAll
+        };
+      } else {
+        return "Input Array Must Contain Only Numbers";
+      }
     } else {
       return "Input Must Contain At Least One Item";
     }
