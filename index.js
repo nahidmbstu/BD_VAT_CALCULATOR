55d86877cf99840fc5b162b716349fce847cffe6
Rakibul Hasan <xenax.rakibul@gmail.com>
2018-08-19 13:15:13 +0600

    Added Inclusive Vat & Exclusive Vat

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports.vatCalculator = function(arr) {
+module.exports.inclusiveVat = function(arr) {
   if (Array.isArray(arr)) {
     if (arr.length > 0) {
       let NewArray = [];
@@ -10,7 +10,37 @@ module.exports.vatCalculator = function(arr) {
       let floatTotalVat = NewArray.reduce((a, b) => Number(a) + Number(b));
       let totalVat = Number.parseFloat(floatTotalVat).toFixed(2);
 
-      let totalSum = arr.map((a, i) => Number(a) + Number(NewArray[i]));
+      let totalSum = arr.map((a, i) => (Number(a) - Number(NewArray[i])).toFixed(2));
+      let TotalAll = totalSum.reduce((a, b) => Number(a) + Number(b));
+
+      return {
+        PreviousItemsPrices: arr,
+        vatOnEachItem: NewArray,
+        TotalVat: totalVat,
+        TotalSumEachItems: totalSum,
+        All: TotalAll
+      };
+    } else {
+      return "Input Must Contain At Least One Item";
+    }
+  } else {
+    return "Input Must Be an Array";
+  }
+};
+
+module.exports.exclusiveVat = function(arr) {
+  if (Array.isArray(arr)) {
+    if (arr.length > 0) {
+      let NewArray = [];
+
+      arr.map(item => {
+        let vat_added = Number(item * (15 / 100)).toFixed(2);
+        NewArray.push(vat_added);
+      });
+      let floatTotalVat = NewArray.reduce((a, b) => Number(a) + Number(b));
+      let totalVat = Number.parseFloat(floatTotalVat).toFixed(2);
+
+      let totalSum = arr.map((a, i) => (Number(a) + Number(NewArray[i])).toFixed(2));
       let TotalAll = totalSum.reduce((a, b) => Number(a) + Number(b));
 
       return {
